f267ad17bdc52368c2e2ffbd80119bb59ea82bd3
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-27 18:18:51 +0530

    Added tests for fetchQuestions

--- src/routes/fetchQuestions.js
+++ src/routes/fetchQuestions.js
@@ -1,3 +1,12 @@
+const externals = require('../externals.json');
+const models = require('../../models');
+
+module.exports.fetchFromExternal = () => Promise.resolve([]);
+module.exports.saveIntoDb = questions => models.questions.upsert(questions);
+module.exports.isDbPopulated = () => Promise.resolve(true);
+module.exports.populateDb = () => Promise.resolve([]);
+module.exports.fetchQuestions = () => Promise.resolve([]);
+
 module.exports.route = {
   path: '/fetchQuestions',
   method: 'GET',
--- test/routes/fetchQuestions.test.js
+++ test/routes/fetchQuestions.test.js
@@ -1,4 +1,28 @@
+const models = require('../../models');
+const {
+  isDbPopulated,
+  populateDb,
+  fetchQuestions,
+} = require('../../src/routes/fetchQuestions');
+
 describe('The fetchQuestions route should', () => {
-  it('populate the database if it is empty and then return the questions');
-  it('fetch questions from database and return it');
+  it('check if db is populated', async () => {
+    await models.questions.destroy({ truncate: true })
+      .then(isDbPopulated)
+      .then((result) => {
+        expect(result).toBeFalsy();
+      });
+  });
+  it('fetch questions from external server, save and return it', async () => {
+    await populateDb()
+      .then((questions) => {
+        expect(questions.length).toBeGreaterThan(0);
+      });
+  });
+  it('return saved questions if present', async () => {
+    await fetchQuestions()
+      .then((questions) => {
+        expect(questions.length).toBeGreaterThan(0);
+      });
+  });
 });
