2f85f4a8c49af645002727fde4a3e0fdce87892e
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-27 19:09:07 +0530

    Implemented login

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,12 +1,45 @@
-const isUserInDb = user => Promise.resolve(true);
-const createUser = user => Promise.resolve({});
-const getUserData = user => Promise.resolve({});
+const models = require('../../models');
+const joi = require('joi');
+
+const getUserData = user => models.users.findOne({
+  where: {
+    username: user.username,
+  },
+});
+
+const isUserInDb = user => getUserData(user)
+  .then(retrievedUser => retrievedUser !== null);
+
+const createUser = user => models.users.create({
+  username: user.username,
+  score: 0,
+});
 
 const route = {
   path: '/login',
   method: 'POST',
   handler: (req, res) => {
-    res('NOT IMPLEMENTED');
+    const { username } = req.headers;
+    const userObj = { username };
+    isUserInDb(userObj)
+      .then((userIsInDb) => {
+        if (userIsInDb) {
+          return getUserData(userObj);
+        }
+        return createUser(userObj)
+          .then(() => getUserData(userObj));
+      })
+      .then(res);
+  },
+  config: {
+    validate: {
+      headers: {
+        username: joi.string().required(),
+      },
+      options: {
+        allowUnknown: true,
+      },
+    },
   },
 };
 
