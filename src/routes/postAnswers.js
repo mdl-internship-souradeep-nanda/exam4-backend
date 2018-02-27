972af7889ed799b368d1dcd804272bf0196a1db2
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-27 19:40:43 +0530

    added postAnswers tests

--- src/routes/postAnswers.js
+++ src/routes/postAnswers.js
@@ -1,7 +1,16 @@
-module.exports.route = {
+const postAnswers = answers => Promise.resolve(true);
+const getScore = user => Promise.resolve(0);
+
+const route = {
   path: '/postAnswers',
   method: 'POST',
   handler: (req, res) => {
     res('NOT IMPLEMENTED');
   },
+};
+
+module.exports = {
+  route,
+  postAnswers,
+  getScore,
 };
--- test/routes/postAnswers.test.js
+++ test/routes/postAnswers.test.js
@@ -1,5 +1,46 @@
+const models = require('../../models');
+
+const { postAnswers, getScore } = require('../../src/routes/postAnswers');
+
 describe('The postAnswers route should', () => {
-  it('check if username field is supplied');
-  it('check if username field is valid');
-  it('compute score based on answers supplied');
+  it('reject if username field is invalid', async () => {
+    const answers = [{
+      username: 'INVALID_USERNAME',
+      questionId: 0,
+      answer: 'answer',
+    }];
+
+    await postAnswers(answers)
+      .then((isSuccess) => {
+        expect(isSuccess).toBeFalsy();
+      });
+  });
+  it('reject if question id is invalid', async () => {
+    const answers = [{
+      username: 'user1',
+      questionId: -1,
+      answer: 'answer',
+    }];
+
+    await models.users.create({ username: 'user1' })
+      .then(() => postAnswers(answers)
+        .then((isSuccess) => {
+          expect(isSuccess).toBeFalsy();
+        }));
+  });
+  it('compute score based on answers supplied', async () => {
+    const answers = [{
+      username: 'user1',
+      questionId: 0,
+      answer: 'answer0',
+    }, {
+      username: 'user1',
+      questionId: 1,
+      answer: 'answer1',
+    }];
+    await postAnswers(answers)
+      .then(() => getScore(answers[0].username)).then((score) => {
+        expect(score).toBe(1);
+      });
+  });
 });
